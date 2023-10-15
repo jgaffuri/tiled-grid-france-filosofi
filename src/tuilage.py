12f455c2ec822375bb380b94ddea26dd4612c04f
jgaffuri <jgaf@proton.me>
2023-10-15 14:53:36 +0200

    a

--- src/tuilage.py
+++ src/tuilage.py
@@ -3,15 +3,14 @@ import subprocess
 # /home/juju/pythonvenvgridDE/bin/python ./src/tuilage.py /usr/bin/python3 /home/juju/workspace/tiled-grid-germany-zensus2011/src/tuilage.py
 
 
-# increase javascript heap size
-# export NODE_OPTIONS="--max-old-space-size=16384"
-# subprocess.run(['export NODE_OPTIONS="--max-old-space-size=16384"'])
+#ind
+#log
+#men
+#pop
 
 
 def tuilage(year, geo, a, t, crs, x, y):
     print(year + " " +geo+ " " + str(a * 200) + "m")
-
-    # gridtiler -i ./input/out_ALTER_KURZ.csv -r 100 -c 3035 -x 3900000 -y 2600000 -p "const a = c.grd_id.split('N')[1].split('E'); return { x:100*a[1],y:100*a[0] };" -m "delete c.grd_id" -a 1 -o ./out/ALTER_KURZcsv/100m/ -e csv
 
     subprocess.run(
         [
@@ -27,7 +26,7 @@ def tuilage(year, geo, a, t, crs, x, y):
             "-y",
             y,
             "-p",
-            "const a = c.grd_id.split('N')[1].split('E'); return { x:a[1],y:a[0] };", #TODO check that
+            "const a = c.grd_id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
             "-m",
             "delete c.id",
             "-a",
