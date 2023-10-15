9e7ee271f274a956d653714801e587f5e8e20901
jgaffuri <jgaf@proton.me>
2023-10-15 09:57:59 +0200

    a

--- src/preparation.py
+++ src/preparation.py
@@ -7,8 +7,7 @@ import numpy as np
 import os  
 
 
-
-
+# prepare les données de 2017
 def prepare2017(geo, printfinal):
 
     print("Charge 2017 " + geo)
@@ -35,6 +34,7 @@ def prepare2017(geo, printfinal):
 prepare2017("reun", True)
 
 
+# prepare les données de 2019
 def prepare2019(geo, printfinal):
 
     print("Charge 2017 " + geo)
@@ -60,50 +60,3 @@ def prepare2019(geo, printfinal):
 #prepare2019("met", False)
 
 
-
-
-
-
-
-
-
-def prepare(csvfile, sep, code, printfinal):
-    print(code)
-
-    print("Load data")
-    df = pd.read_csv(csvfile, sep=sep, encoding="iso-8859-1") #, nrows=10000)
-
-    print("drop unecessary rows")
-    df = df[df.Merkmal == code]
-
-    print("drop unecessary columns")
-    df = df.drop(["Gitter_ID_100m_neu", "Auspraegung_Text", "Anzahl_q", "Merkmal"], axis=1)
-
-    print("Make new grd_id column")
-    df["grd_id"] = df.apply(
-        lambda row: row["Gitter_ID_100m"].replace("100m", ""), axis=1
-    )
-
-    print("Drop unecessary column Gitter_ID_100m")
-    df = df.drop(["Gitter_ID_100m"], axis=1)
-
-    print("Rename Anzahl column")
-    df = df.rename(columns={"Anzahl": "value"})
-
-    print("Pivot")
-    df = pd.pivot_table(
-        df,
-        columns=["Auspraegung_Code"],
-        values="value",
-        index=["grd_id"],
-        aggfunc=np.sum,
-        fill_value=0,
-    )
-
-    if printfinal:
-        print(df)
-
-    print("Save")
-    df.to_csv("input/out_" + code + ".csv")
-
-    print("Done " + code)
