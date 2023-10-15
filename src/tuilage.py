8284953e272662a92abc1863d373f9df885824ef
jgaffuri <jgaf@proton.me>
2023-10-15 17:04:11 +0200

    columns

--- src/tuilage.py
+++ src/tuilage.py
@@ -3,39 +3,57 @@ import subprocess
 # /home/juju/pythonvenvgridDE/bin/python ./src/tuilage.py /usr/bin/python3 /home/juju/workspace/tiled-grid-germany-zensus2011/src/tuilage.py
 
 
-#ind
-#log
-#men
-#pop
+def getParams(year, geo, a, t, crs, x, y, outFolder):
+    return [
+        "gridtiler",
+        "-i",
+        "./tmp/" + year + "_" + geo + ".csv",
+        "-r",
+        "200",
+        "-c",
+        crs,
+        "-x",
+        x,
+        "-y",
+        y,
+        "-p",
+        "const a = c.grd_id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
+        "-m",
+        "delete c.id",
+        "-a",
+        str(a),
+        "-o",
+        "./out/csv/" + outFolder + "/" + year + "/" + geo + "/" + str(a * 200) + "m/",
+        "-t",
+        str(t),
+        "-e",
+        "csv",
+    ]
 
 
 def tuilage(year, geo, a, t, crs, x, y):
-    print(year + " " +geo+ " " + str(a * 200) + "m")
+    print(year + " " + geo + " " + str(a * 200) + "m")
 
-    subprocess.run(
-        [
-            "gridtiler",
-            "-i",
-            "./tmp/"+year+"_" + geo + ".csv",
-            "-r",
-            "200",
-            "-c",
-            crs,
-            "-x",
-            x,
-            "-y",
-            y,
-            "-p",
-            "const a = c.grd_id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
-            "-m",
-            "delete c.id",
-            "-a",
-            str(a),
-            "-o",
-            "./out/csv/" + year + "/" + geo + "/" + str(a * 200) + "m/",
-            "-t",
-            str(t),
-            "-e",
-            "csv",
-        ]
-    )
+    # ind
+    params = getParams(year, geo, a, t, crs, x, y, "ind")
+    params.push("-s")
+    params.push("id,imputed,ind,ind_0_3,ind_11_17,ind_18_24,ind_25_39,ind_40_54,ind_4_5,ind_55_64,ind_65_79,ind_6_10,ind_80p,ind_inc")
+    subprocess.run(params)
+
+    # log
+    # id,imputed,ind,
+    # log_45_70,log_70_90,log_ap90,log_av45,log_inc,log_soc
+
+    # men
+    # id,imputed,ind,
+    # men,men_1ind,men_5ind,men_coll,men_fmp,men_mais,men_pauv,men_prop,men_surf
+
+    # pop_inc
+    # id,imputed,ind,
+    # ind_snv,
+
+
+
+
+for a in [1, 2, 5, 10, 20, 50, 100, 200, 500]:
+    tuilage("2019", "met", a, 128, "3035", 0, 0)
