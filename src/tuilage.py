fa56cbb8b476e0f1abb3427bbc0ac6a1cd0c5950
jgaffuri <jgaf@proton.me>
2023-10-15 22:49:11 +0200

    a

--- src/tuilage.py
+++ src/tuilage.py
@@ -7,93 +7,81 @@ import subprocess
 # subprocess.run(['export NODE_OPTIONS="--max-old-space-size=16384"'])
 
 
-# Construit une liste de paramètres par défaut pour gridtiler
-def getParams(year, geo, a, t, crs, x, y, rounding, outFolder):
-    return [
-        "gridtiler",
-        "-i",
-        "./tmp/" + str(year) + "_" + geo + ".csv",
-        "-r",
-        "200",
-        "-c",
-        crs,
-        "-x",
-        str(x),
-        "-y",
-        str(y),
-        "-p",
-        "const a = c.id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
-        "-m",
-        "delete c.id",
-        "-a",
-        str(a),
-        "-o",
-        "./out/csv/"
-        + geo
-        + "/"
-        + outFolder
-        + "/"
-        + str(year)
-        + "/"
-        + str(a * 200)
-        + "m/",
-        "-t",
-        str(t),
-        "-R",
-        str(rounding),
-        "-e",
-        "csv",
-    ]
+def tuilage(year, geo, a, rounding, ind):
+    # défini les paramètres du tuilage en fonction de l'indicateur
+    if ind == "ind":
+        t = 128
+        cols = "id,imputed,ind,ind_0_3,ind_11_17,ind_18_24,ind_25_39,ind_40_54,ind_4_5,ind_55_64,ind_65_79,ind_6_10,ind_80p,ind_inc"
+    elif ind == "log":
+        t = 128
+        cols = "id,imputed,ind,log_45_70,log_70_90,log_ap90,log_av45,log_inc,log_soc"
+    elif ind == "men":
+        t = 128
+        cols = "id,imputed,ind,men,men_1ind,men_5ind,men_coll,men_fmp,men_mais,men_pauv,men_prop,men_surf"
+    elif ind == "inc":
+        t = 256
+        cols = "id,imputed,ind,ind_snv"
 
+    # défini les paramètres du tuilage en fonction du territoire géographique
+    if geo == "met":
+        crs = "3035"
+        x = 3200000
+        y = 2000000
+    elif geo == "reun":
+        crs = "2975"
+        x = 300000
+        y = 7600000
+    elif geo == "mart":
+        crs = "5490"
+        x = 600000
+        y = 1500000
 
-#
-def tuilage(year, geo, a, crs, x, y):
-    print("*** " + geo + " " + str(year) + " " + str(a * 200) + "m")
-
-    # population, par âge
-    params = getParams(year, geo, a, 128, crs, x, y, 2, "ind")
-    params.append("-s")
-    params.append(
-        "id,imputed,ind,ind_0_3,ind_11_17,ind_18_24,ind_25_39,ind_40_54,ind_4_5,ind_55_64,ind_65_79,ind_6_10,ind_80p,ind_inc"
+    # execute tuilage, via gridtiler
+    subprocess.run(
+        [
+            "gridtiler",
+            "-i",
+            "./tmp/" + str(year) + "_" + geo + ".csv",
+            "-r",
+            "200",
+            "-c",
+            crs,
+            "-x",
+            str(x),
+            "-y",
+            str(y),
+            "-p",
+            "const a = c.id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
+            "-m",
+            "delete c.id",
+            "-a",
+            str(a),
+            "-o",
+            "./out/csv/"
+            + geo
+            + "/"
+            + ind
+            + "/"
+            + str(year)
+            + "/"
+            + str(a * 200)
+            + "m/",
+            "-t",
+            str(t),
+            "-s",
+            cols,
+            "-R",
+            str(rounding),
+            "-e",
+            "csv",
+        ]
     )
-    subprocess.run(params)
-
-    # logements
-    params = getParams(year, geo, a, 128, crs, x, y, 2, "log")
-    params.append("-s")
-    params.append(
-        "id,imputed,ind,log_45_70,log_70_90,log_ap90,log_av45,log_inc,log_soc"
-    )
-    subprocess.run(params)
-
-    # menages
-    params = getParams(year, geo, a, 128, crs, x, y, 2, "men")
-    params.append("-s")
-    params.append(
-        "id,imputed,ind,men,men_1ind,men_5ind,men_coll,men_fmp,men_mais,men_pauv,men_prop,men_surf"
-    )
-    subprocess.run(params)
-
-    # population et revenus
-    params = getParams(year, geo, a, 256, crs, x, y, 2, "inc")
-    params.append("-s")
-    params.append("id,imputed,ind,ind_snv")
-    subprocess.run(params)
 
 
-# lance le tuilage pour tous les territoires geographiques, toutes les années et toutes les résolution
+# lance le tuilage pour tous les territoires geographiques, toutes les années, tous les indicateurs et toutes les résolution
 
-# reunion
-for year in [2019, 2017, 2015]:
-    for a in [1, 2, 5, 10, 25, 50, 100, 250, 500]:
-        tuilage(year, "reun", a, "2975", 300000, 7600000)
-
-# martinique
-for year in [2019, 2017, 2015]:
-    for a in [1, 2, 5, 10, 25, 50, 100, 250, 500]:
-        tuilage(year, "mart", a, "5490", 600000, 1500000)
-
-# metropole
-for year in [2019, 2017, 2015]:
-    for a in [1, 2, 5, 10, 25, 50, 100, 250, 500]:
-        tuilage(year, "met", a, "3035", 3200000, 2000000)
+for geo in ["reun", "mart", "met"]:
+    for year in [2019, 2017, 2015]:
+        for ind in ["ind", "log", "men", "inc"]:
+            for a in [1, 2, 5, 10, 25, 50, 100, 250, 500]:
+                tuilage(year, geo, a, 2, ind)
