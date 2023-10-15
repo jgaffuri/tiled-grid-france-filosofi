036583266eedde66f4f78a1aae14c792be79debb
jgaffuri <jgaf@proton.me>
2023-10-15 11:12:27 +0200

    a

--- src/preparation.py
+++ src/preparation.py
@@ -7,17 +7,18 @@ import numpy as np
 import os  
 
 
-# prepare les données de 2017
-def prepare2017(geo, printfinal):
+def prepare(year, geo, printfinal):
 
-    print("Charge 2017 " + geo)
-    df = pd.read_csv("input/2017_"+geo+".csv") #, nrows=10000)
+    print("Charge "+year+" " + geo)
+    df = pd.read_csv("input/"+year+"_"+geo+".csv") #, nrows=10000)
 
     print("Supprime colonnes inutilisées")
-    df = df.drop(["Idcar_1km", "Idcar_nat", "I_est_1km", "lcog_geo", "Groupe"], axis=1)
+    colSup = ["Idcar_1km", "Idcar_nat", "I_est_1km", "lcog_geo", "Groupe"] if year == "2017" else ["idcar_1km", "idcar_nat", "i_est_1km", "lcog_geo"] if year == "2019" else []
+    df = df.drop(colsSup, axis=1)
 
     print("Renomme colonne id")
-    df = df.rename(columns={"Idcar_200m": "id"})
+    colId = "Idcar_200m" if year == "2017" else "idcar_200m" if year == "2019" else ""
+    df = df.rename(columns={colId: "id"})
 
     print("Change noms colonnes en lettres minuscules")
     df.columns = df.columns.str.lower()
@@ -29,45 +30,17 @@ def prepare2017(geo, printfinal):
 
     print("Sauvegarde")
     if not os.path.exists('tmp'): os.makedirs('tmp')
-    df.to_csv("tmp/2017_"+geo+".csv", index=False)
+    df.to_csv("tmp/"+year+"_"+geo+".csv", index=False)
 
-    print("Fait - 2017 " + geo)
-
-
-prepare2017("reun", False)
-prepare2017("mart", False)
-prepare2017("met", False)
+    print("Fait - "+year+" " + geo)
 
 
-# prepare les données de 2019
-def prepare2019(geo, printfinal):
 
-    print("Charge 2017 " + geo)
-    df = pd.read_csv("input/2019_"+geo+".csv") #, nrows=10000)
-
-    print("Supprime colonnes inutilisées")
-    df = df.drop(["idcar_1km", "idcar_nat", "i_est_1km", "lcog_geo"], axis=1)
-
-    print("Renomme colonne id")
-    df = df.rename(columns={"idcar_200m": "id"})
-
-    print("Change noms colonnes en lettres minuscules")
-    df.columns = df.columns.str.lower()
-
-    print("Classe colonnes par ordre alphabétique")
-    df = df.reindex(sorted(df.columns), axis=1)
-
-    if(printfinal): print(df)
-
-    print("Sauvegarde")
-    if not os.path.exists('tmp'): os.makedirs('tmp')
-    df.to_csv("tmp/2019_"+geo+".csv", index=False)
-
-    print("Fait - 2019 " + geo)
-
-
-prepare2019("reun", False)
-prepare2019("mart", False)
-prepare2019("met", False)
+prepare("2017", "reun", False)
+prepare("2017", "mart", False)
+prepare("2017", "met", False)
+prepare("2019", "reun", False)
+prepare("2019", "mart", False)
+prepare("2019", "met", False)
 
 
