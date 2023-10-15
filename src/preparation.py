68e55e401a657fe86fdd18130addf53783e48241
jgaffuri <jgaf@proton.me>
2023-10-15 09:56:42 +0200

    a

--- src/preparation.py
+++ src/preparation.py
@@ -11,22 +11,25 @@ import os
 
 def prepare2017(geo, printfinal):
 
-    print("Load data 2017 " + geo)
+    print("Charge 2017 " + geo)
     df = pd.read_csv("input/2017_"+geo+".csv") #, nrows=10000)
 
-    print("Drop unecessary columns")
+    print("Supprime colonnes inutilisées")
     df = df.drop(["Idcar_1km", "Idcar_nat", "I_est_1km", "lcog_geo", "Groupe"], axis=1)
 
-    print("Rename id column")
+    print("Renomme colonne id")
     df = df.rename(columns={"Idcar_200m": "id"})
+
+#en capitale
+#ordonne les colonnes par ordre alphabetique
 
     if(printfinal): print(df)
 
-    print("Save")
+    print("Sauvegarde")
     if not os.path.exists('tmp'): os.makedirs('tmp')
     df.to_csv("tmp/2017_"+geo+".csv", index=False)
 
-    print("Done 2017 " + geo)
+    print("Fait - 2017 " + geo)
 
 
 prepare2017("reun", True)
@@ -34,22 +37,22 @@ prepare2017("reun", True)
 
 def prepare2019(geo, printfinal):
 
-    print("Load data 2019 " + geo)
+    print("Charge 2017 " + geo)
     df = pd.read_csv("input/2019_"+geo+".csv") #, nrows=10000)
 
-    print("drop unecessary columns")
+    print("Supprime colonnes inutilisées")
     df = df.drop(["idcar_1km", "idcar_nat", "i_est_1km", "lcog_geo"], axis=1)
 
-    print("Rename id column")
+    print("Renomme colonne id")
     df = df.rename(columns={"idcar_200m": "id"})
 
     if(printfinal): print(df)
 
-    print("Save")
+    print("Sauvegarde")
     if not os.path.exists('tmp'): os.makedirs('tmp')
     df.to_csv("tmp/2019_"+geo+".csv", index=False)
 
-    print("Done 2019 " + geo)
+    print("Fait - 2019 " + geo)
 
 
 #prepare2019("reun", False)
