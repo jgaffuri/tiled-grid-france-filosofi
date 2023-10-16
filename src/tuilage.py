48c2180453d9a1dfdcd004499251ae5330032fd0
jgaffuri <jgaf@proton.me>
2023-10-16 10:11:33 +0200

    add 600m resolution

--- src/tuilage.py
+++ src/tuilage.py
@@ -83,6 +83,7 @@ def tuilage(year, geo, a, rounding, ind):
 for geo in ["reun", "mart", "met"]:
     for year in [2019, 2017, 2015]:
         for ind in ["ind", "log", "men", "inc"]:
-            for a in [1, 2, 5, 10, 25, 50, 100, 250, 500]:
+            #for a in [1, 2, 3, 5, 10, 25, 50, 100, 250, 500]:
+            for a in [3]:
                 print("*** " + geo + " " + year + " " + ind + " " + a)
                 tuilage(year, geo, a, 2, ind)
