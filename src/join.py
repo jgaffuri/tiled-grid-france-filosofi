796c4c70869139f8070aaeb74f01cc92393fb84f
jgaffuri <jgaf@proton.me>
2023-10-22 21:01:09 +0200

    join

--- src/join.py
+++ src/join.py
@@ -2,19 +2,38 @@ import pandas as pd
 
 # /home/juju/pythonvenvgridDE/bin/python ./src/join.py /usr/bin/python3 /home/juju/workspace/tiled-grid-france-filosofi/src/join.py
 
-# read
-geo = "met"
-d19 = pd.read_csv("./tmp/2019_" + geo + ".csv")
-d17 = pd.read_csv("./tmp/2017_" + geo + ".csv")
-d15 = pd.read_csv("./tmp/2015_" + geo + ".csv")
 
+def extract(year, geo, printfinal=False):
+    print("load " + year + " " + geo)
+    d = pd.read_csv("./tmp/" + year + "_" + geo + ".csv")
+    print("select columns")
+    d = d[["id", "ind", "imputed"]]
+    print("rename columns")
+    d = d.rename(columns={"ind": "ind" + year, "imputed": "imp" + year})
+    if printfinal:
+        print(d)
+
+
+def prepareJoin(geo, printfinal=False):
+    d = extract("2019", geo)
+    d = pd.merge(d, extract("2017", geo), on="id", how="inner")
+    d = pd.merge(d, extract("2015", geo), on="id", how="inner")
+    if printfinal:
+        print(d)
+    return d
+
+prepareJoin("met", True)
+
+
+# for geo in ["reun", "mart", "met"]:
+# geo = "met"
 
 
 # join 1
-#result1 = pd.merge(df1, df2, on='common_column1', how='inner')
+# result1 = pd.merge(df1, df2, on='id', how='inner')
 
 # join 2
-#result2 = pd.merge(result1, df3, on='common_column2', how='inner')
+# result2 = pd.merge(result1, df3, on='id', how='inner')
 
 # save
-#result2.to_csv('out.csv', index=False)
+# result2.to_csv('out.csv', index=False)
