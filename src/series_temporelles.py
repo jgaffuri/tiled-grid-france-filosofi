228be614ead507b501f84e3eeae85fadf1404a77
jgaffuri <jgaf@proton.me>
2023-10-22 21:48:30 +0200

    time series data

--- src/series_temporelles.py
+++ src/series_temporelles.py
@@ -35,54 +35,55 @@ for col in ["ind", "ind_snv"]:
 
 
 # Tuilage, via gridtiler
-for geo in []:
-#for geo in ["reun", "mart", "met"]:
-    t = 128
-    rounding = 2
+for col in ["ind", "ind_snv"]:
+    for geo in []:
+    #for geo in ["reun", "mart", "met"]:
+        t = 128
+        rounding = 2
 
-    # défini les paramètres du tuilage en fonction du territoire géographique
-    if geo == "met":
-        crs = "3035"
-        x = 3200000
-        y = 2000000
-    elif geo == "reun":
-        crs = "2975"
-        x = 300000
-        y = 7600000
-    elif geo == "mart":
-        crs = "5490"
-        x = 600000
-        y = 1500000
+        # défini les paramètres du tuilage en fonction du territoire géographique
+        if geo == "met":
+            crs = "3035"
+            x = 3200000
+            y = 2000000
+        elif geo == "reun":
+            crs = "2975"
+            x = 300000
+            y = 7600000
+        elif geo == "mart":
+            crs = "5490"
+            x = 600000
+            y = 1500000
 
-    for a in [1, 2, 3, 5, 10, 25, 50, 100, 250, 500]:
-        print("*** " + geo + " " + str(a * 200) + "m")
+        for a in [1, 2, 3, 5, 10, 25, 50, 100, 250, 500]:
+            print("*** " + geo + " " + str(a * 200) + "m")
 
-        subprocess.run(
-            [
-                "gridtiler",
-                "-i",
-                "./tmp/ts_" + col + "_" + geo + ".csv",
-                "-r",
-                "200",
-                "-c",
-                crs,
-                "-x",
-                str(x),
-                "-y",
-                str(y),
-                "-p",
-                "const a = c.id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
-                "-m",
-                "delete c.id",
-                "-a",
-                str(a),
-                "-o",
-                "./out/csv/" + geo + "/ts_" + col + "/" + str(a * 200) + "m/",
-                "-t",
-                str(t),
-                "-R",
-                str(rounding),
-                "-e",
-                "csv",
-            ]
-        )
+            subprocess.run(
+                [
+                    "gridtiler",
+                    "-i",
+                    "./tmp/ts_" + col + "_" + geo + ".csv",
+                    "-r",
+                    "200",
+                    "-c",
+                    crs,
+                    "-x",
+                    str(x),
+                    "-y",
+                    str(y),
+                    "-p",
+                    "const a = c.id.split('N')[1].split('E'); return { x:a[1],y:a[0] };",
+                    "-m",
+                    "delete c.id",
+                    "-a",
+                    str(a),
+                    "-o",
+                    "./out/csv/" + geo + "/ts_" + col + "/" + str(a * 200) + "m/",
+                    "-t",
+                    str(t),
+                    "-R",
+                    str(rounding),
+                    "-e",
+                    "csv",
+                ]
+            )
