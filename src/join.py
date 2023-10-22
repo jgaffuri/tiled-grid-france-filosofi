970d7b8dd0065fdb6610bf6e314019cfe4601daa
jgaffuri <jgaf@proton.me>
2023-10-22 21:12:04 +0200

    ts

--- src/join.py
+++ src/join.py
@@ -6,34 +6,25 @@ import pandas as pd
 def extract(year, geo, printfinal=False):
     print("load " + year + " " + geo)
     d = pd.read_csv("./tmp/" + year + "_" + geo + ".csv")
-    print("select columns")
+    #print("select columns")
     d = d[["id", "ind", "imputed"]]
-    print("rename columns")
+    #print("rename columns")
     d = d.rename(columns={"ind": "ind" + year, "imputed": "imp" + year})
-    if printfinal:
-        print(d)
-
-
-def prepareJoin(geo, printfinal=False):
-    d = extract("2019", geo)
-    d = pd.merge(d, extract("2017", geo), on="id", how="inner")
-    d = pd.merge(d, extract("2015", geo), on="id", how="inner")
     if printfinal:
         print(d)
     return d
 
-prepareJoin("met", True)
+
+def prepareJoin(geo, printfinal=False):
+    d = extract("2019", geo)
+    d = pd.merge(d, extract("2017", geo), on="id", how="outer")
+    d = pd.merge(d, extract("2015", geo), on="id", how="outer")
+    if printfinal:
+        print(d)
+    return d
 
 
 # for geo in ["reun", "mart", "met"]:
-# geo = "met"
-
-
-# join 1
-# result1 = pd.merge(df1, df2, on='id', how='inner')
-
-# join 2
-# result2 = pd.merge(result1, df3, on='id', how='inner')
-
-# save
-# result2.to_csv('out.csv', index=False)
+geo = "met"
+d = prepareJoin(geo, True)
+d.to_csv("./tmp/ts_pop_" + geo + ".csv", index=False)
