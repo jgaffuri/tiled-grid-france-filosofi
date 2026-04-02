1d2125bf2813fcded2b1dc88b0da6b4db438d0f0
jgaffuri <jgaf@proton.me>
2026-04-02 19:55:40 +0200

    a

--- src/2_aggregation.py
+++ src/2_aggregation.py
@@ -5,6 +5,6 @@ for geo in ["mart","reun","met"]:
     for year in ["2015","2017","2019","2021"]:
         for a in [2,3,5,10,25,50,100,250,500]:
             res = int(a*200)
-            print("*** "+year+" "+geo+" "+res)
+            print("*** "+year+" "+geo+" "+str(res))
             gridtiler.grid_aggregation(input_file="tmp/"+year+"_"+geo+"_200.csv", resolution=200, output_file="tmp/"+year+"_"+geo+"_"+str(res)+".csv", a=a)
 
