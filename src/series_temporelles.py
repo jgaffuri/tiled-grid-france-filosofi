9947d17f02ef7f0cd6499b1b82724834c96eb6ac
jgaffuri <jgaf@proton.me>
2026-04-04 11:28:01 +0200

    a

--- src/series_temporelles.py
+++ src/series_temporelles.py
@@ -6,8 +6,8 @@ import subprocess
 def extraction(year, geo, cols, res, renameFun, printfinal=False):
     print("charge " + year + " " + geo)
     d = pd.read_csv("./tmp/" + year + "_" + geo + "_" + str(res) + ".csv")
-    d = d[cols]
     d["id"] = d["x"].astype(str) + "_" + d["y"].astype(str)
+    d = d[cols]
     d = d.rename(columns=renameFun(year))
     if printfinal:
         print(d)
@@ -23,7 +23,7 @@ def jointure(geo, code, cols, res, renameFun, printfinal=False):
     if printfinal:
         print(d)
     # Sauvegarde
-    d.to_csv("./tmp/ts_" + code + "_" + geo + "_" + res + ".csv", index=False)
+    d.to_csv("./tmp/ts_" + code + "_" + geo + "_" + str(res) + ".csv", index=False)
 
 
 # Préparation des données des séries temporelles, par région
